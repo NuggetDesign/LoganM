8f868b91fbb7bdd146d43cb5229b0e1f82b17230
NuggetDesign <nuggetssignature@gmail.com>
2023-05-29 02:34:19 +0700

    tablet

--- index.js
+++ index.js
@@ -63,15 +63,64 @@ function initializeAccordion() {
       reviewCarousel.refresh(); // Refresh the Splide instance
     });
 
-    // Custom button functionality
-    var prevButtonReview = document.getElementById('review-prev');
-    var nextButtonReview = document.getElementById('review-next');
+    // // Custom button functionality
+    // var prevButtonReview = document.getElementById('review-prev');
+    // var nextButtonReview = document.getElementById('review-next');
 
-    prevButtonReview.addEventListener('click', function() {
-      reviewCarousel.go('-');
-    });
+    // prevButtonReview.addEventListener('click', function() {
+    //   reviewCarousel.go('-');
+    // });
 
-    nextButtonReview.addEventListener('click', function() {
-      reviewCarousel.go('+');
-    });
+    // nextButtonReview.addEventListener('click', function() {
+    //   reviewCarousel.go('+');
+    // });
   })();
+
+  function navigationButton() {
+    const navButton = document.getElementById('navButton');
+    const menu = document.getElementById('menu');
+    const links = menu.getElementsByTagName('a');
+  
+    const tl = gsap.timeline({ paused: true });
+  
+    tl.fromTo(
+      menu,
+      { opacity: 0, paddingTop: 0, paddingBottom: 0 },
+      { opacity: 1, paddingTop: '64px', paddingBottom: '16px', duration: 0.3, ease: 'power2.out' }
+    );
+    tl.fromTo(
+      menu,
+      { clipPath: 'inset(0 0 100% 0)' },
+      { clipPath: 'inset(0 0 0% 0)', duration: 0.3, ease: 'power2.out' }
+    );
+  
+    navButton.addEventListener('click', function() {
+      navButton.classList.toggle('active');
+      menu.classList.toggle('active');
+  
+      if (menu.classList.contains('active')) {
+        menu.style.height = '';
+        menu.style.visibility = 'visible';
+        menu.style.clipPath = 'inset(0 0 100% 0)';
+        tl.play();
+      } else {
+        tl.reverse().then(() => {
+          menu.style.height = '0';
+          menu.style.visibility = 'hidden';
+        });
+      }
+    });
+  
+    Array.from(links).forEach(function(link) {
+      link.addEventListener('click', function() {
+        navButton.classList.remove('active');
+        menu.classList.remove('active');
+        tl.reverse().then(() => {
+          menu.style.height = '0';
+          menu.style.visibility = 'hidden';
+        });
+      });
+    });
+  }
+  
+  navigationButton();
